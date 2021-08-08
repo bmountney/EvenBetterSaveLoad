07cf095f9ecd36ce0c03adae06fbd89d3410bd03
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2021-08-09 00:24:45 +0800

    Add files via upload

---
+++ BetterSaveLoadPatch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using TaleWorlds.Core;
+
+namespace BetterSaveLoad
+{
+    [HarmonyPatch(typeof(MBSaveLoad))]
+    public static class BetterSaveLoadPatch
+    {
+        // Get the name of the currently loaded save file.
+        [HarmonyPatch("LoadSaveGameData")]
+        public static void Postfix(string ___ActiveSaveSlotName) => ActiveSaveSlotName = ___ActiveSaveSlotName;
+        // Increment the quick save index. Replace the save file name with a custom one with the quick save index.
+        // Display the file name of the saved game in a debug message.
+        [HarmonyPatch("QuickSaveCurrentGame")]
+        public static void Prefix(ref string ___ActiveSaveSlotName)
+        {
+            QuickSaveIndex++;
+            ___ActiveSaveSlotName = QuickSaveNamePrefix + QuickSaveIndex;
+            InformationManager.DisplayMessage(new InformationMessage("Game saved: \"" + ___ActiveSaveSlotName + "\"."));
+        }
+        // Set the quick save index to the highest number in the list of quick save file names.
+        // Display the file name of the loaded game in a debug message.
+        public static void InitializeQuickSaveIndex()
+        {
+            List<int> list = new List<int>();
+            foreach (string text in MBSaveLoad.GetSaveFileNames())
+            {
+                if (text.Contains(QuickSaveNamePrefix))
+                {
+                    string[] array = text.Split(new char[] { '_' });
+                    if (array.Length == 3 && int.TryParse(array[array.Length - 1], out int num) && num > 0)
+                    {
+                        list.Add(num);
+                    }
+                }
+            }
+            if (list.Count > 0)
+            {
+                QuickSaveIndex = list.Max();
+            }
+            InformationManager.DisplayMessage(new InformationMessage("Game loaded: \"" + ActiveSaveSlotName + "\"."));
+        }
+        private static int QuickSaveIndex = 0;
+        private static string QuickSaveNamePrefix = "save_quick_";
+        private static string ActiveSaveSlotName = null;
+    }
+}
---
+++ BetterSaveLoadSubModule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Engine.Screens;
+using TaleWorlds.InputSystem;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.SaveSystem;
+using TaleWorlds.SaveSystem.Load;
+using SandBox;
+using SandBox.View.Map;
+
+namespace BetterSaveLoad
+{
+    // This mod adds functionality for quick loading and incremental quick saving.
+    public class BetterSaveLoadSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad() => new Harmony("mod.bannerlord.bettersaveload").PatchAll();
+        public override void OnGameLoaded(Game game, object initializerObject) => BetterSaveLoadPatch.InitializeQuickSaveIndex();
+        // Quick save or quick load when the respective keys are pressed.
+        protected override void OnApplicationTick(float dt)
+        {
+            if (ScreenManager.TopScreen is MapScreen)
+            {
+                if (Input.IsKeyDown(InputKey.LeftControl) || Input.IsKeyDown(InputKey.RightControl))
+                {
+                    if (Input.IsKeyDown(InputKey.S) && !_isSaving)
+                    {
+                        Campaign.Current.SaveHandler.QuickSaveCurrentGame();
+                        _isSaving = true;
+                    }
+                    else if (Input.IsKeyDown(InputKey.L) && !_isLoading)
+                    {
+                        QuickLoadPreviousGame();
+                        _isLoading = true;
+                    }
+                }
+                else
+                {
+                    _isSaving = false;
+                    _isLoading = false;
+                }
+                if (Input.IsKeyPressed(InputKey.F9))
+                {
+                    QuickLoadPreviousGame();
+                }
+            }
+        }
+        // Get the latest quick save, manual save or auto save.
+        public void QuickLoadPreviousGame()
+        {
+            SaveGameFileInfo[] saveFiles = MBSaveLoad.GetSaveFiles(null);
+            if (Extensions.IsEmpty<SaveGameFileInfo>(saveFiles))
+            {
+                InformationManager.DisplayMessage(new InformationMessage("No save files to load!"));
+            }
+            SaveHelper.TryLoadSave(Extensions.MaxBy<SaveGameFileInfo, DateTime>(saveFiles, (SaveGameFileInfo s) => TaleWorlds.Core.MetaDataExtensions.GetCreationTime(s.MetaData)), new Action<LoadResult>(StartGame), null);
+        }
+        private void StartGame(LoadResult loadResult)
+        {
+            ScreenManager.PopScreen();
+            GameStateManager.Current.CleanStates(0);
+            GameStateManager.Current = Module.CurrentModule.GlobalGameStateManager;
+            MBGameManager.StartNewGame(new SandBoxGameManager(loadResult));
+        }
+        private bool _isSaving;
+        private bool _isLoading;
+    }
+}
