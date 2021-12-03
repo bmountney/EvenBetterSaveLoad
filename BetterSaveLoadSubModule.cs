431cd4eea96abc97b898e2dccbecbe2dc00420be
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2021-12-04 00:07:31 +0800

    Fixed a bug where the pre-battle auto save would trigger when the player talked to a friendly party leader.

---
+++ BetterSaveLoadSubModule.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Engine.Screens;
+using TaleWorlds.InputSystem;
+using TaleWorlds.MountAndBlade;
+using SandBox.View.Map;
+
+namespace BetterSaveLoad
+{
+    // This mod adds functionality for quick loading and incremental quick saving, as well as auto saving before battles.
+    public class BetterSaveLoadSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad() => new Harmony("mod.bannerlord.bettersaveload").PatchAll();
+        protected override void OnGameStart(Game game, IGameStarter gameStarter)
+        {
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter campaignStarter = (CampaignGameStarter)gameStarter;
+                campaignStarter.AddBehavior(new BetterSaveLoadBehavior());
+            }
+        }
+        // Quick save or quick load when the respective keys are pressed.
+        protected override void OnApplicationTick(float dt)
+        {
+            if (ScreenManager.TopScreen is MapScreen)
+            {
+                if (Input.IsKeyDown(InputKey.LeftControl) || Input.IsKeyDown(InputKey.RightControl))
+                {
+                    if (Input.IsKeyPressed(InputKey.S))
+                    {
+                        Campaign.Current.SaveHandler.QuickSaveCurrentGame();
+                    }
+                    else if (Input.IsKeyPressed(InputKey.L))
+                    {
+                        BetterSaveLoadManager.QuickLoadPreviousGame();
+                    }
+                }
+                if (Input.IsKeyPressed(InputKey.F9))
+                {
+                    BetterSaveLoadManager.QuickLoadPreviousGame();
+                }
+            }
+        }
+    }
+}
