e8c517048fcd7061ce4ba717f501bc257b897139
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2024-02-16 02:30:14 +0800

    Fixed a bug where a quick save would be created when pressing S.

---
+++ BetterSaveLoadMissionBehavior.cs
@@ -0,0 +1,18 @@
+﻿using TaleWorlds.InputSystem;
+using TaleWorlds.MountAndBlade;
+
+namespace BetterSaveLoad
+{
+    public class BetterSaveLoadMissionBehavior : MissionBehavior
+    {
+        public override MissionBehaviorType BehaviorType => MissionBehaviorType.Other;
+
+        public override void OnMissionTick(float dt)
+        {
+            if (Mission.InputManager.IsControlDown() && Mission.InputManager.IsKeyPressed(InputKey.L))
+            {
+                BetterSaveLoadManager.QuickLoadPreviousGame();
+            }
+        }
+    }
+}
--- BetterSaveLoadSubModule.cs
+++ BetterSaveLoadSubModule.cs
@@ -1,75 +1,58 @@
-﻿using Bannerlord.ButterLib.HotKeys;
-using HarmonyLib;
+﻿using HarmonyLib;
 using SandBox;
 using SandBox.View.Map;
 using System;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
+using TaleWorlds.InputSystem;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.View.Screens;
 using TaleWorlds.SaveSystem.Load;
 using TaleWorlds.ScreenSystem;
-using static BetterSaveLoad.BetterSaveLoadHotKeys;
 
 namespace BetterSaveLoad
 {
     // This mod adds functionality for quick loading and incremental quick saving, as well as auto saving before and after battles.
     public class BetterSaveLoadSubModule : MBSubModuleBase
     {
-        private bool _isHotKeyManagerCreated;
-
         protected override void OnSubModuleLoad() => new Harmony("mod.bannerlord.bettersaveload").PatchAll();
-
-        protected override void OnBeforeInitialModuleScreenSetAsRoot()
-        {
-            if (!_isHotKeyManagerCreated)
-            {
-                HotKeyManager hotKeyManager = HotKeyManager.Create("BetterSaveLoad");
-                LCtrl lCtrl = hotKeyManager.Add<LCtrl>();
-                RCtrl rCtrl = hotKeyManager.Add<RCtrl>();
-                S s = hotKeyManager.Add<S>();
-                L l = hotKeyManager.Add<L>();
-                F9 f9 = hotKeyManager.Add<F9>();
-
-                s.Predicate = () => Campaign.Current != null && ScreenManager.TopScreen is MapScreen;
-                l.Predicate = () => Campaign.Current != null && (ScreenManager.TopScreen is MapScreen || ScreenManager.TopScreen is MissionScreen);
-                f9.Predicate = () => Campaign.Current != null && ScreenManager.TopScreen is MapScreen;
-
-                lCtrl.OnPressedEvent += () =>
-                {
-                    s.IsEnabled = true;
-                    l.IsEnabled = true;
-                };
-                lCtrl.OnReleasedEvent += () =>
-                {
-                    s.IsEnabled = false;
-                    l.IsEnabled = false;
-                };
-                rCtrl.OnPressedEvent += () =>
-                {
-                    s.IsEnabled = true;
-                    l.IsEnabled = true;
-                };
-                rCtrl.OnReleasedEvent += () =>
-                {
-                    s.IsEnabled = false;
-                    l.IsEnabled = false;
-                };
-                s.OnPressedEvent += () => Campaign.Current.SaveHandler.QuickSaveCurrentGame();
-                l.OnPressedEvent += () => BetterSaveLoadManager.QuickLoadPreviousGame();
-                f9.OnPressedEvent += () => BetterSaveLoadManager.QuickLoadPreviousGame();
-
-                hotKeyManager.Build();
-
-                _isHotKeyManagerCreated = true;
-            }
-        }
 
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
         {
             if (game.GameType is Campaign)
             {
                 ((CampaignGameStarter)gameStarterObject).AddBehavior(new BetterSaveLoadCampaignBehavior());
+            }
+        }
+
+        protected override void OnApplicationTick(float dt)
+        {
+            if (Campaign.Current != null)
+            {
+                IInputContext input = MapScreen.Instance?.Input ?? Mission.Current?.InputManager;
+
+                if (input != null)
+                {
+                    bool isMapScreen = ScreenManager.TopScreen is MapScreen, isMissionScreen = ScreenManager.TopScreen is MissionScreen;
+
+                    if (input.IsControlDown())
+                    {
+                        if (input.IsKeyPressed(InputKey.S) && isMapScreen)
+                        {
+                            Campaign.Current.SaveHandler.QuickSaveCurrentGame();
+                        }
+
+                        if (input.IsKeyPressed(InputKey.L) && (isMapScreen || isMissionScreen))
+                        {
+                            BetterSaveLoadManager.QuickLoadPreviousGame();
+                        }
+                    }
+
+                    if (input.IsKeyPressed(InputKey.F9) && isMapScreen)
+                    {
+                        BetterSaveLoadManager.QuickLoadPreviousGame();
+                    }
+                }
             }
         }
 
