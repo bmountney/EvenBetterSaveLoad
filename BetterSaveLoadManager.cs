ba4b6de5cd1545007f2c5023f4c12bb45f1831b1
Bill Mountney <bill@mountney.com>
2024-03-14 10:25:02 -0400

    Added additional options for save file names to make them cleaner and to optionally use a time stamp instead of a numerical counter. Also added the ability to have the native auto-saves follow the same naming format.

--- BetterSaveLoadManager.cs
+++ BetterSaveLoadManager.cs
@@ -1,6 +1,10 @@
 ﻿using HarmonyLib;
 using SandBox;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.MapEvents;
 using TaleWorlds.Core;
@@ -9,6 +13,7 @@ using TaleWorlds.Localization;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.SaveSystem;
 using TaleWorlds.SaveSystem.Load;
+using System.Text.RegularExpressions;
 
 namespace BetterSaveLoad
 {
@@ -16,16 +21,17 @@ namespace BetterSaveLoad
     public static class BetterSaveLoadManager
     {
         private static readonly BetterSaveLoadSettings Settings = BetterSaveLoadSettings.Instance;
-        private static readonly string QuickSaveNamePrefix = "save_quick_", BattleAutoSaveNamePrefix = "save_auto_battle_";
+        //private static readonly string QuickSaveNamePrefix = "", BattleAutoSaveNamePrefix = "";
 
-        private static int QuickSaveIndex = 0, BattleAutoSaveIndex = 0;
+        private static int QuickSaveIndex = 0, AutoSaveIndex = 0, PreBattleSaveIndex = 0, PostBattleSaveIndex = 0;
         private static string ActiveSaveSlotName = null;
 
         public static SaveGameFileInfo SaveFile { get; set; }
 
         public static bool CanLoad => SaveFile != null && !SaveFile.IsCorrupted;
 
-        public static string PlayerClanAndMainHeroName => Clan.PlayerClan.Name.ToString().ToLower() + "_" + Hero.MainHero.Name.ToString().ToLower() + "_";
+        //public static string PlayerClanAndMainHeroName => Clan.PlayerClan.Name.ToString().ToLower() + "_" + Hero.MainHero.Name.ToString().ToLower() + "_";
+        public static string PlayerClanAndMainHeroName => Clan.PlayerClan.Name.ToString() + " " + Hero.MainHero.Name.ToString();
 
         // Get the name of the currently loaded save file.
         [HarmonyPostfix]
@@ -39,19 +45,21 @@ namespace BetterSaveLoad
         [HarmonyPatch("QuickSaveCurrentGame")]
         public static void Prefix(ref string ___ActiveSaveSlotName)
         {
-            // Increment the quick save index.
-            QuickSaveIndex++;
-
-            if (Settings.ShouldLimitSaves && QuickSaveIndex > Settings.QuickSaveLimit)
+            string saveFileIndex;
+            if (Settings.ShouldUseIndex)
             {
-                // Reset the quick save index if it is greater than the maximum number in the settings.
-                QuickSaveIndex = 1;
+                if (++QuickSaveIndex > Settings.QuickSaveLimit && Settings.ShouldLimitSaves)
+                {
+                    // Reset the quick save index if it is greater than the maximum number in the settings.
+                    QuickSaveIndex = 1;
+                }
+                saveFileIndex = GetQuickSaveFileIndex();
             }
-
-            // Replace the save file name with a custom one with the quick save index.
-            ___ActiveSaveSlotName = QuickSaveNamePrefix + PlayerClanAndMainHeroName + QuickSaveIndex;
-
-            ActiveSaveSlotName = ___ActiveSaveSlotName;
+            else
+            {
+                saveFileIndex = GetSaveFileTimeStamp();
+            }
+            ActiveSaveSlotName = ___ActiveSaveSlotName = GetSaveFileBaseName() + saveFileIndex + Settings.QuickSaveFileNameSuffix;
 
             // Display the file name of the saved game in a debug message.
             InformationManager.DisplayMessage(new InformationMessage(new TextObject("{=BSLmsg001}Game saved: \"{SAVENAME}\".").SetTextVariable("SAVENAME", ActiveSaveSlotName).ToString()));
@@ -59,24 +67,54 @@ namespace BetterSaveLoad
 
         public static void InitializeSaveIndexes()
         {
-            string quickSaveName = string.Empty, battleAutoSaveName = string.Empty, quickSaveNameWithoutIndex = QuickSaveNamePrefix + PlayerClanAndMainHeroName, battleAutoSaveNameWithoutIndex = BattleAutoSaveNamePrefix + PlayerClanAndMainHeroName;
+            string quickSaveName = string.Empty, autoSaveName = string.Empty, preBattleSaveName = string.Empty, postBattleSaveName = string.Empty,
+                saveBaseName = GetSaveFileBaseName(),
+                quickSaveSearchName = WildCardToRegular(GetSaveFileBaseName() + "*" + Settings.QuickSaveFileNameSuffix),
+                autoSaveSearchName = WildCardToRegular(GetSaveFileBaseName() + "*" + Settings.AutoSaveFileNameSuffix),
+                preBattleSaveSearchName = WildCardToRegular(GetSaveFileBaseName() + "*" + Settings.PreBattleFileNameSuffix),
+                postBasttleSaveSearchName = WildCardToRegular(GetSaveFileBaseName() + "*" + Settings.PostBattleFileNameSuffix),
+                quickSaveNameIndex, autoSaveNameIndex, preBattleSaveNameIndex, postBattleSaveNameIndex;
 
-            quickSaveName = MBSaveLoad.GetSaveFiles().FirstOrDefault(saveFile => saveFile.Name.StartsWith(quickSaveNameWithoutIndex))?.Name;
-            battleAutoSaveName = MBSaveLoad.GetSaveFiles().FirstOrDefault(saveFile => saveFile.Name.StartsWith(battleAutoSaveNameWithoutIndex))?.Name;
+            quickSaveName = MBSaveLoad.GetSaveFiles().FirstOrDefault(saveFile => Regex.IsMatch(saveFile.Name, quickSaveSearchName))?.Name;
+            autoSaveName = MBSaveLoad.GetSaveFiles().FirstOrDefault(saveFile => Regex.IsMatch(saveFile.Name, autoSaveSearchName))?.Name;
+            preBattleSaveName = MBSaveLoad.GetSaveFiles().FirstOrDefault(saveFile => Regex.IsMatch(saveFile.Name, preBattleSaveSearchName))?.Name;
+            postBattleSaveName = MBSaveLoad.GetSaveFiles().FirstOrDefault(saveFile => Regex.IsMatch(saveFile.Name, postBasttleSaveSearchName))?.Name;
 
             QuickSaveIndex = 0;
-            BattleAutoSaveIndex = 0;
+            AutoSaveIndex = 0;
+            PreBattleSaveIndex = 0;
+            PostBattleSaveIndex = 0;
 
-            if (!string.IsNullOrEmpty(quickSaveName) && int.TryParse(quickSaveName.Substring(quickSaveNameWithoutIndex.Length), out int quickSaveIndex) && quickSaveIndex > 0 && quickSaveIndex <= Settings.QuickSaveLimit)
+            if (Settings.ShouldUseIndex)
             {
-                // Set the quick save index to the number in the latest quick save file name.
-                QuickSaveIndex = quickSaveIndex;
-            }
-
-            if (!string.IsNullOrEmpty(battleAutoSaveName) && int.TryParse(battleAutoSaveName.Substring(battleAutoSaveNameWithoutIndex.Length), out int battleAutoSaveIndex) && battleAutoSaveIndex > 0 && battleAutoSaveIndex <= Settings.BattleAutoSaveLimit)
-            {
-                // Set the battle auto save index to the number in the latest battle auto save file name.
-                BattleAutoSaveIndex = battleAutoSaveIndex;
+                if (!string.IsNullOrEmpty(quickSaveName))
+                {
+                    // Set the quick save index to the number in the latest quick save file name.
+                    quickSaveNameIndex = quickSaveName.Substring(saveBaseName.Length, quickSaveName.Length - saveBaseName.Length - Settings.QuickSaveFileNameSuffix.Length).TrimStart('0');
+                    if (int.TryParse(quickSaveNameIndex, out int quickSaveIndex) && quickSaveIndex > 0 && quickSaveIndex <= Settings.QuickSaveLimit)
+                        QuickSaveIndex = quickSaveIndex;
+                }
+                if (!string.IsNullOrEmpty(autoSaveName))
+                {
+                    // Set the auto save index to the number in the latest quick save file name.
+                    autoSaveNameIndex = autoSaveName.Substring(saveBaseName.Length, autoSaveName.Length - saveBaseName.Length - Settings.AutoSaveFileNameSuffix.Length).TrimStart('0');
+                    if (int.TryParse(autoSaveNameIndex, out int autoSaveIndex) && autoSaveIndex > 0 && autoSaveIndex <= Settings.AutoSaveLimit)
+                        AutoSaveIndex = autoSaveIndex;
+                }
+                if (!string.IsNullOrEmpty(preBattleSaveName))
+                {
+                    // Set the pre save index to the number in the latest quick save file name.
+                    preBattleSaveNameIndex = preBattleSaveName.Substring(saveBaseName.Length, preBattleSaveName.Length - saveBaseName.Length - Settings.PreBattleFileNameSuffix.Length).TrimStart('0');
+                    if (int.TryParse(preBattleSaveNameIndex, out int preBattleSaveIndex) && preBattleSaveIndex > 0 && preBattleSaveIndex <= Settings.PreBattleSaveLimit)
+                        PreBattleSaveIndex = preBattleSaveIndex;
+                }
+                if (!string.IsNullOrEmpty(postBattleSaveName))
+                {
+                    // Set the post save index to the number in the latest quick save file name.
+                    postBattleSaveNameIndex = postBattleSaveName.Substring(saveBaseName.Length, postBattleSaveName.Length - saveBaseName.Length - Settings.PostBattleFileNameSuffix.Length).TrimStart('0');
+                    if (int.TryParse(postBattleSaveNameIndex, out int postBattleSaveIndex) && postBattleSaveIndex > 0 && postBattleSaveIndex <= Settings.PostBattleSaveLimit)
+                        PostBattleSaveIndex = postBattleSaveIndex;
+                }
             }
 
             if (ActiveSaveSlotName != null)
@@ -94,16 +132,36 @@ namespace BetterSaveLoad
             // Execute only if the numbers of attackers and defenders are greater than or equal to the minimum numbers in the settings.
             if (Settings.ShouldAutoSaveForBattle && (shouldAutoSaveBeforeBattle || shouldAutoSaveAfterBattle))
             {
-                // Increment the battle auto save index.
-                BattleAutoSaveIndex++;
-
-                if (Settings.ShouldLimitSaves && BattleAutoSaveIndex > Settings.BattleAutoSaveLimit)
+                string saveFileIndex = null, saveFileSuffix = null;
+                if (Settings.ShouldUseIndex)
                 {
-                    // Reset the battle auto save index if it is greater than the maximum number in the settings.
-                    BattleAutoSaveIndex = 1;
+                    if (shouldAutoSaveBeforeBattle)
+                    {
+                        if (++PreBattleSaveIndex > Settings.PreBattleSaveLimit && Settings.ShouldLimitSaves)
+                        {
+                            // Reset the before battle auto save index if it is greater than the maximum number in the settings.
+                            PreBattleSaveIndex = 1;
+                        }
+                        saveFileIndex = GetPreBattleSaveFileIndex();
+                        saveFileSuffix = Settings.PreBattleFileNameSuffix;
+                    }
+                    else if (shouldAutoSaveAfterBattle)
+                    {
+                        if (++PostBattleSaveIndex > Settings.PostBattleSaveLimit && Settings.ShouldLimitSaves)
+                        {
+                            // Reset the after battle auto save index if it is greater than the maximum number in the settings.
+                            PostBattleSaveIndex = 1;
+                        }
+                        saveFileIndex = GetPostBattleSaveFileIndex();
+                        saveFileSuffix = Settings.PostBattleFileNameSuffix;
+                    }
                 }
-
-                ActiveSaveSlotName = BattleAutoSaveNamePrefix + PlayerClanAndMainHeroName + BattleAutoSaveIndex;
+                else
+                {
+                    saveFileIndex = GetSaveFileTimeStamp();
+                    saveFileSuffix = (shouldAutoSaveBeforeBattle ? Settings.PreBattleFileNameSuffix : Settings.PostBattleFileNameSuffix);
+                }
+                ActiveSaveSlotName = GetSaveFileBaseName() + saveFileIndex + saveFileSuffix;
 
                 Campaign.Current.SaveHandler.SaveAs(ActiveSaveSlotName);
                 // Display the file name of the saved game in a debug message.
@@ -131,6 +189,65 @@ namespace BetterSaveLoad
             SaveFile = null;
 
             MBGameManager.StartNewGame(new SandBoxGameManager(loadResult));
+        }
+
+        [HarmonyPatch("GetAutoSaveName")]
+        public static string Postfix(string ___result)
+        {
+            string saveFileIndex;
+            if (Settings.ShouldUseIndex)
+            {
+                if (++AutoSaveIndex > Settings.QuickSaveLimit && Settings.ShouldLimitSaves)
+                {
+                    // Reset the save index if it is greater than the maximum number in the settings.
+                    AutoSaveIndex = 1;
+                }
+                saveFileIndex = GetAutoSaveFileIndex();
+            }
+            else
+            {
+                saveFileIndex = GetSaveFileTimeStamp();
+            }
+            return GetSaveFileBaseName() + saveFileIndex + Settings.AutoSaveFileNameSuffix;
+        }
+
+        public static string GetSaveFileBaseName()
+        {
+            return Settings.SaveFileNamePrefix + PlayerClanAndMainHeroName + " ";
+        }
+
+        public static string GetQuickSaveFileIndex()
+        {
+            return QuickSaveIndex.ToString("N0").PadLeft(Settings.IndexZeroPadding + 1, '0');
+        }
+
+        public static string GetAutoSaveFileIndex()
+        {
+            return AutoSaveIndex.ToString("N0").PadLeft(Settings.IndexZeroPadding + 1, '0');
+        }
+
+        public static string GetPreBattleSaveFileIndex()
+        {
+            return PreBattleSaveIndex.ToString("N0").PadLeft(Settings.IndexZeroPadding + 1, '0');
+        }
+
+        public static string GetPostBattleSaveFileIndex()
+        {
+            return PostBattleSaveIndex.ToString("N0").PadLeft(Settings.IndexZeroPadding + 1, '0');
+        }
+
+        public static string GetSaveFileTimeStamp()
+        {
+            string timeStamp;
+            DateTime ts = DateTime.Now;
+            timeStamp = $"{ts.Year:0000}" + $"{ts.Month:00}" + $"{ts.Day:00}" + $"-{ts.Hour:00}" + $"{ts.Minute:00}" + $"{ts.Second:00}"; // + $"-{ts.Millisecond:000}";
+
+            return $"{timeStamp}";
+        }
+
+        private static String WildCardToRegular(String value)
+        {
+            return "^" + Regex.Escape(value).Replace("\\?", ".").Replace("\\*", ".*") + "$";
         }
     }
 }
--- BetterSaveLoadSettings.cs
+++ BetterSaveLoadSettings.cs
@@ -8,40 +8,76 @@ namespace BetterSaveLoad
 {
     public class BetterSaveLoadSettings : AttributeGlobalSettings<BetterSaveLoadSettings>
     {
-        public override string Id => "BetterSaveLoad";
+        public override string Id => "EvenBetterSaveLoad";
 
-        public override string DisplayName => "Better Save/Load";
+        public override string DisplayName => "Even Better Save/Load";
 
-        public override string FolderName => "BetterSaveLoad";
+        public override string FolderName => "EvenBetterSaveLoad";
 
         public override string FormatType => "json2";
 
-        [SettingPropertyBool("{=BSLopt001}Toggle Save Limits", Order = 0, RequireRestart = false, HintText = "{=BSLopt001Hint}Limit number of quick saves and battle auto saves. Enabled by default.", IsToggle = true)]
-        [SettingPropertyGroup("{=BSLoptg001}Save Limits", GroupOrder = 0)]
+        [SettingPropertyBool("{=BSLopt001}Incremental File Index", Order = 0, RequireRestart = false, HintText = "{=BSLopt001Hint}When enabled an incremental index is used for save files, otherwise a time stamp in the format YYYYMMDD-HHMMSS is used. Enabled by default.", IsToggle = true)]
+        [SettingPropertyGroup("{=BSLoptg001}Incremental File Index (turn off to use time stamp)", GroupOrder = 0)]
+        public bool ShouldUseIndex { get; set; } = true;
+
+        [SettingPropertyInteger("{=BSLopt002}Index Zero Padding", 0, 4, Order = 1, RequireRestart = false, HintText = "{=BSLopt002Hint}Pad index with zeros. Default is 2.")]
+        [SettingPropertyGroup("{=BSLoptg001}Incremental File Index (turn off to use time stamp)", GroupOrder = 0)]
+        public int IndexZeroPadding { get; set; } = 2;
+
+        [SettingPropertyBool("{=BSLopt003}Toggle Save Limits", Order = 2, RequireRestart = false, HintText = "{=BSLopt003Hint}Limit number of quick saves and battle auto saves. Enabled by default.", IsToggle = true)]
+        [SettingPropertyGroup("{=BSLoptg001}Incremental File Index (turn off to use time stamp)/Save Limits", GroupOrder = 0)]
         public bool ShouldLimitSaves { get; set; } = true;
 
-        [SettingPropertyInteger("{=BSLopt002}Quick Save Limit", 1, 100, "0", Order = 1, RequireRestart = false, HintText = "{=BSLopt002Hint}Maximum number of quick saves. Default is 3.")]
-        [SettingPropertyGroup("{=BSLoptg001}Save Limits", GroupOrder = 0)]
+        [SettingPropertyInteger("{=BSLopt004}Quick Save Limit", 1, 999, "0", Order = 3, RequireRestart = false, HintText = "{=BSLopt004Hint}Maximum number of quick saves. Default is 3.")]
+        [SettingPropertyGroup("{=BSLoptg001}Incremental File Index (turn off to use time stamp)/Save Limits", GroupOrder = 0)]
         public int QuickSaveLimit { get; set; } = 3;
 
-        [SettingPropertyInteger("{=BSLopt003}Battle Auto Save Limit", 1, 100, "0", Order = 2, RequireRestart = false, HintText = "{=BSLopt003Hint}Maximum number of battle auto saves. Default is 3.")]
-        [SettingPropertyGroup("{=BSLoptg001}Save Limits", GroupOrder = 0)]
-        public int BattleAutoSaveLimit { get; set; } = 3;
+        [SettingPropertyInteger("{=BSLopt005}Auto Save Limit", 1, 999, "0", Order = 4, RequireRestart = false, HintText = "{=BSLopt005Hint}Maximum number of auto saves. Default is 3.")]
+        [SettingPropertyGroup("{=BSLoptg001}Incremental File Index (turn off to use time stamp)/Save Limits", GroupOrder = 0)]
+        public int AutoSaveLimit { get; set; } = 3;
 
-        [SettingPropertyBool("{=BSLopt004}Toggle Battle Auto Save", Order = 0, RequireRestart = false, HintText = "{=BSLopt004Hint}Auto save for battles. Enabled by default.", IsToggle = true)]
+        [SettingPropertyInteger("{=BSLopt006}Pre-Battle Auto Save Limit", 1, 999, "0", Order = 5, RequireRestart = false, HintText = "{=BSLopt006Hint}Maximum number of pre-battle auto saves. Default is 3.")]
+        [SettingPropertyGroup("{=BSLoptg001}Incremental File Index (turn off to use time stamp)/Save Limits", GroupOrder = 0)]
+        public int PreBattleSaveLimit { get; set; } = 3;
+
+        [SettingPropertyInteger("{=BSLopt007}Post-Battle Auto Save Limit", 1, 999, "0", Order = 6, RequireRestart = false, HintText = "{=BSLopt007Hint}Maximum number of post-battle auto saves. Default is 3.")]
+        [SettingPropertyGroup("{=BSLoptg001}Incremental File Index (turn off to use time stamp)/Save Limits", GroupOrder = 0)]
+        public int PostBattleSaveLimit { get; set; } = 3;
+
+        [SettingPropertyBool("{=BSLopt008}Toggle Battle Auto Save", Order = 0, RequireRestart = false, HintText = "{=BSLopt008Hint}Auto save for battles. Enabled by default.", IsToggle = true)]
         [SettingPropertyGroup("{=BSLoptg002}Battle Auto Save", GroupOrder = 1)]
         public bool ShouldAutoSaveForBattle { get; set; } = true;
 
-        [SettingPropertyDropdown("{=BSLopt005}Trigger", Order = 1, RequireRestart = false, HintText = "{=BSLopt005Hint}When to trigger battle auto saves. Default is Pre-battle.")]
+        [SettingPropertyDropdown("{=BSLopt009}Trigger", Order = 1, RequireRestart = false, HintText = "{=BSLopt009Hint}When to trigger battle auto saves. Default is Pre-battle.")]
         [SettingPropertyGroup("{=BSLoptg002}Battle Auto Save", GroupOrder = 1)]
         public Dropdown<TextObject> BattleAutoSaveTrigger { get; set; } = new Dropdown<TextObject>(new TextObject[] { new TextObject("{=BSLoptv001}Pre-battle"), new TextObject("{=BSLoptv002}Post-battle"), new TextObject("{=BSLoptv003}Both") }, 0);
 
-        [SettingPropertyInteger("{=BSLopt006}Minimum Attacker Troop Count", 1, 1000, "0", Order = 2, RequireRestart = false, HintText = "{=BSLopt006Hint}Minimum number of attacking troops to trigger auto saving before and after battles. Default is 50.")]
+        [SettingPropertyInteger("{=BSLopt010}Minimum Attacker Troop Count", 1, 1000, "0", Order = 2, RequireRestart = false, HintText = "{=BSLopt010Hint}Minimum number of attacking troops to trigger auto saving before and after battles. Default is 50.")]
         [SettingPropertyGroup("{=BSLoptg002}Battle Auto Save", GroupOrder = 1)]
         public int MinAttackerTroopCount { get; set; } = 50;
 
-        [SettingPropertyInteger("{=BSLopt007}Minimum Defender Troop Count", 1, 1000, "0", Order = 3, RequireRestart = false, HintText = "{=BSLopt007Hint}Minimum number of defending troops to trigger auto saving before and after battles. Default is 50.")]
+        [SettingPropertyInteger("{=BSLopt011}Minimum Defender Troop Count", 1, 1000, "0", Order = 3, RequireRestart = false, HintText = "{=BSLopt011Hint}Minimum number of defending troops to trigger auto saving before and after battles. Default is 50.")]
         [SettingPropertyGroup("{=BSLoptg002}Battle Auto Save", GroupOrder = 1)]
         public int MinDefenderTroopCount { get; set; } = 50;
+
+        [SettingPropertyText("{=BSLopt012}Save File Name Prefix", Order = 0, RequireRestart = false, HintText = "{=BSLopt012Hint}Prefix on all save file names.")]
+        [SettingPropertyGroup("{=BSLoptg003}File Name Format", GroupOrder = 2)]
+        public string SaveFileNamePrefix { get; set; } = "Save ";
+
+        [SettingPropertyText("{=BSLopt013}Quick Save File Name Suffix", Order = 1, RequireRestart = false, HintText = "{=BSLopt013Hint}Suffix on quick save file name.")]
+        [SettingPropertyGroup("{=BSLoptg003}File Name Format", GroupOrder = 2)]
+        public string QuickSaveFileNameSuffix { get; set; } = " Quick";
+
+        [SettingPropertyText("{=BSLopt014}Auto Save File Name Suffix", Order = 2, RequireRestart = false, HintText = "{=BSLopt014Hint}Suffix on auto save file name.")]
+        [SettingPropertyGroup("{=BSLoptg003}File Name Format", GroupOrder = 2)]
+        public string AutoSaveFileNameSuffix { get; set; } = " Auto";
+
+        [SettingPropertyText("{=BSLopt015}Before Battle Save File Name Suffix", Order = 3, RequireRestart = false, HintText = "{=BSLopt015Hint}Suffix on battle auto save file name.")]
+        [SettingPropertyGroup("{=BSLoptg003}File Name Format", GroupOrder = 2)]
+        public string PreBattleFileNameSuffix { get; set; } = " Before";
+
+        [SettingPropertyText("{=BSLopt016}After Battle Save File Name Suffix", Order = 4, RequireRestart = false, HintText = "{=BSLopt016Hint}Suffix on battle auto save file name.")]
+        [SettingPropertyGroup("{=BSLoptg003}File Name Format", GroupOrder = 2)]
+        public string PostBattleFileNameSuffix { get; set; } = " After";
     }
 }
